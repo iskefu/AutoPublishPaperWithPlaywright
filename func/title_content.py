c3ae3e879109c6031a2f114a4473e8ea8dc815a5
kefu1252 <17765251252@163.com>
2024-04-27 04:54:02 +0800

    完成B站的自动化发布，B站不支持md格式，转docx   python sendkeys错误

--- func/title_content.py
+++ func/title_content.py
@@ -1,11 +1,33 @@
 import os
 import re
+
+import mistune
 def title(filepath):
     filename = os.path.splitext(os.path.basename(filepath))[0]
     return filename 
 def content(filepath):
+   
     with open(filepath, 'r',encoding='utf-8') as f:
         content = f.read()
-        content=re.sub(r'---(.|\s)*?---', '', content)
+    content=re.sub(r'---(.|\s)*?---', '', content)
     return content
- 
+
+def md_content(filepath):
+    markdown = mistune.create_markdown()
+    with open(filepath, 'r',encoding='utf-8') as f:
+        content = f.read()
+    html = markdown(content) 
+    print(html)
+
+    return content
+
+
+
+
+
+if __name__ == '__main__':
+    
+    filepath = '/run/media/kf/data/obsidian/Capture/ollma3部署记录.md'
+
+    # content(filepath)
+    md_content(filepath)
--- seleniumtest.py
+++ seleniumtest.py
@@ -5,7 +5,7 @@ from time import sleep
 from selenium.webdriver.common.by import By
 from func.find_click import find_and_click, find_input, find_upload, get_herf, markdownhere, wait_login_success
 from func.md_to_doc import md_to_docx
-from func.title_content import title, content
+from func.title_content import content, md_content, title
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
@@ -56,14 +56,30 @@ def ZHIHU(platform="zhihu"):
         print("发布成功")    
     
 def BLBL(platform="bilibili"):
+    
     driver.get(config[platform]["url"])
     swich_new_window(driver)
-    sleep(2)
-    el=driver.find_element(By.XPATH,config[platform]["title"])
-    print(el)
-    find_input(driver,config[platform]["title"],title)
-    find_input(driver,config[platform]["content"],content)
-    find_and_click(driver,config[platform]["publish"])
+    sleep(3)
+    
+    iframe= driver.find_element(By.XPATH,"//div[@id='edit-article-box']//iframe")
+    driver.switch_to.frame(iframe)
+    print("switch to iframe")
+    sleep(1)
+    
+    driver.find_element(By.XPATH,"//textarea").send_keys(title)
+    print("title")
+    sleep(1)
+
+
+    driver.find_element(By.XPATH,"//div[@data-placeholder='请输入正文']").send_keys(content)
+    print("content")
+    sleep(5)
+
+    e=driver.find_element(By.XPATH,"//button[contains(text(),'提交文章')]")
+    driver.execute_script('arguments[0].scrollIntoView({block: "center"});',e)
+    e.click()
+    print("submit")
+    sleep(1)
     
 def CSDN(platform="csdn"):
     driver.get(config[platform]["url"])
@@ -103,7 +119,11 @@ def WXGZH(platform="wxgzh"):
     find_input(driver,config[platform]["content"],content)
     markdownhere(driver,config[platform]["content"])
     driver.switch_to.default_content()
-    
+def js_click(driver,xpath):
+    e=driver.find_element(By.XPATH,xpath)
+    driver.execute_script('arguments[0].scrollIntoView({block: "center"});',e)
+    driver.execute_script("arguments[0].click();", e)
+
 def TOUTIAO(platform='toutiao'):
     
     driver.get(config[platform]["url"])
@@ -124,11 +144,7 @@ def TOUTIAO(platform='toutiao'):
     driver.find_element(By.XPATH,config[platform]["title"]).send_keys(title)
     sleep(1)
     print('标题')
-    def js_click(driver,xpath):
-        e=driver.find_element(By.XPATH,xpath)
-        driver.execute_script('arguments[0].scrollIntoView({block: "center"});',e)
-        driver.execute_script("arguments[0].click();", e)
-    
+
     js_click(driver,"//span[contains(text(),'单标题')]/..")
     sleep(1)
     print('单标题')
@@ -148,33 +164,40 @@ def TOUTIAO(platform='toutiao'):
     
     # 确认发布
     driver.find_element(By.XPATH,config[platform]["confirm_publish"]).click()
+    print('确认发布')
+    sleep(1)
     
-
+    # 手机验证码
+    driver.find_element(By.XPATH,config[platform]["phone_code"]).click()
+    print('手机验证码')
+    sleep(1)
+    
+    
 if __name__ ==  '__main__':
+    
     port=9222
+    
     # 打开浏览器
     # subprocess.Popen(["/usr/bin/google-chrome-stable" ,f"--remote-debugging-port={port}"])
-
+    # sleep(3)
+    
     options = Options()
     # options.add_argument("--disable-gpu")
-    # options.add_argument("--headless")
     options.debugger_address=f"127.0.0.1:{port}"
     driver = webdriver.Chrome(options = options)
-    # driver=webdriver.Chrome()
     # driver.maximize_window()
     driver.implicitly_wait(10) # secondst)
 
     filepath = '/run/media/kf/data/obsidian/Capture/ollma3部署记录.md'
     title = title(filepath)
     content = content(filepath)
-    
     with open('xpath_config.json', 'r') as f:
         config = json.load(f)
     
     # JIANSHU(platform="jianshu")
     # ZHIHU(platform="zhihu")
-    # BLBL(platform="bilibili")
+    BLBL(platform="blbl")
     # CSDN(platform="csdn")
     # WXGZH(platform="wxgzh")
-    TOUTIAO(platform="toutiao")
+    # TOUTIAO(platform="toutiao")
     
